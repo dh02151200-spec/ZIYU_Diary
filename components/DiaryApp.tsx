62a94757af03220bf32e750198998ec8eddb3679
romi <dh02151200@gmail.com>
2026-05-02 23:31:49 +0900

    update design

--- components/DiaryApp.tsx
+++ components/DiaryApp.tsx
@@ -197,13 +197,11 @@ export default function DiaryApp() {
               <div className="binding-spine" aria-hidden="true" />
 
               <div className="diary-page diary-page-left">
-                               <div className="page-paper-grain" />
-                <img src="/images/stickers/tape-blue.svg" className="page-sticker tape tape-letters" alt="" />
-                <img src="/images/stickers/heart.svg" className="page-sticker sticker-heart-right" alt="" />
-                <img src="/images/stickers/ziyufamily.png" className="page-sticker sticker-ziyufamily" alt="" />
-  <img src="/images/stickers/ziyu_fish.gif" className="page-sticker sticker-ziyu_bar" alt="" />
+                <div className="page-paper-grain" />
 
-
+                <img src="/images/stickers/tape-blue.svg" className="page-sticker tape tape-title" alt="" />
+                <img src="/images/stickers/draw_fish.png" className="page-sticker sticker-draw_fish" alt="" />
+                <img src="/images/stickers/flower.svg" className="page-sticker sticker-flower-left" alt="" />
                 <img src="/images/stickers/tape-dot.svg" className="page-sticker tape tape-guide" alt="" />
 
                 <div className="left-page-header">
@@ -289,8 +287,10 @@ export default function DiaryApp() {
                   .join(" ")}
               >
                 <div className="page-paper-grain" />
-               
+                <img src="/images/stickers/tape-blue.svg" className="page-sticker tape tape-letters" alt="" />
+                <img src="/images/stickers/heart.svg" className="page-sticker sticker-heart-right" alt="" />
                 <img src="/images/stickers/ziyufamily.png" className="page-sticker sticker-ziyufamily" alt="" />
+  <img src="/images/stickers/ziyu_fish.gif" className="page-sticker sticker-ziyu_bar" alt="" />
                 <div className="right-page-header">
                   <div>
                     <p className="eyebrow">{COPY.lettersTitle}</p>
